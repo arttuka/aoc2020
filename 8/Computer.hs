e81ae9a6b5e0ab27fd38865a9ab77757c2884564
Arttu Kaipiainen <arttukaipiainen@gmail.com>
2020-12-12 10:03:32 +0200

    Day 8 part 2

--- 8/Computer.hs
+++ 8/Computer.hs
@@ -1,6 +1,7 @@
 module Computer where
 
-import Data.Vector (Vector, (!), fromList)
+import Data.Maybe (mapMaybe)
+import Data.Vector (Vector, (!), (//), fromList)
 import Data.List.Split (splitOn)
 
 data Operation = Acc | Jmp | Nop deriving (Eq, Show)
@@ -31,14 +32,25 @@ readProgram = fromList . fmap readLine
 step :: State -> State
 step state@State{ pointer=pointer, program=program, accumulator=accumulator } = state { accumulator = accumulator + dAcc, pointer = pointer + dPointer }
   where
-    instruction      = program ! pointer
-    (dAcc, dPointer) = case instruction of
+    (dAcc, dPointer) = case program ! pointer of
       (Instruction Acc n) -> (n, 1)
       (Instruction Jmp n) -> (0, n)
       (Instruction Nop _) -> (0, 1)
 
 runProgram :: Program -> [State]
-runProgram program = states
+runProgram program = run State { pointer = 0, accumulator = 0, program = program }
   where
-    initialState = State { pointer = 0, accumulator = 0, program = program }
-    states = initialState : map step states
+    end = length program
+    run :: State -> [State]
+    run state
+        | pointer state == end = [state]
+        | otherwise            = state : run (step state)
+
+alterProgram :: Program -> [Program]
+alterProgram program = mapMaybe alter [0..(length program - 1)]
+  where
+    alter :: Int -> Maybe Program
+    alter i = case program ! i of
+        (Instruction Jmp n) -> Just $ program // [(i, Instruction Nop n)]
+        (Instruction Nop n) -> Just $ program // [(i, Instruction Jmp n)]
+        _                   -> Nothing
--- 8/Main.hs
+++ 8/Main.hs
@@ -1,10 +1,17 @@
 module Main where
 
+import Data.List (find)
 import Data.Set (Set, empty, member, insert)
-import Computer (Program, State, readProgram, runProgram, pointer, accumulator)
+import Computer (Program, State, readProgram, runProgram, pointer, accumulator, alterProgram)
 import Util (readWith)
 
 data Result = Loop State | Terminate State
+isTerminate :: Result -> Bool
+isTerminate (Terminate _) = True
+isTerminate _             = False
+getState :: Result -> State
+getState (Terminate state) = state
+getState (Loop state) = state
 
 run :: Program -> Result
 run program = step empty (runProgram program)
@@ -15,7 +22,13 @@ run program = step empty (runProgram program)
       | pointer s `member` seen = Loop s
       | otherwise               = step (insert (pointer s) seen) ss
 
+runUntilSuccess :: [Program] -> Maybe State
+runUntilSuccess = fmap getState . find isTerminate . fmap run
+
 main :: IO ()
 main = do program <- readWith readProgram
-          let (Loop state) = run program
-          print $ accumulator state
+          let programs = alterProgram program
+          let result = runUntilSuccess programs
+          print $ case result of
+            (Just state) -> show $ accumulator state
+            Nothing      -> "Nothing was found"
