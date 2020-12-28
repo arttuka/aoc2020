588109cd31b3b0aa051d63375f4d7a2558369c0e
Arttu Kaipiainen <arttukaipiainen@gmail.com>
2020-12-28 04:17:24 +0200

    Day 22 part 2

--- 22/Main.hs
+++ 22/Main.hs
@@ -1,26 +1,45 @@
 module Main where
 
-import Prelude hiding (null)
+import Prelude hiding (length, null, take)
 import Control.Monad (liftM2)
-import Queue (Queue, empty, fromList, null, peek, pop, pushAll, toList)
+import Data.Set (Set, insert, member)
+import qualified Data.Set as Set
+import Queue (Queue, empty, fromList, length, null, peek, pop, pushAll, take, toList)
+import qualified Queue
 import Util (readGroupsWith)
 
-readDeck :: [String] -> Queue Int
+type Deck = Queue Int
+type Decks = (Deck, Deck)
+type SeenDecks = Set Decks
+
+readDeck :: [String] -> Deck
 readDeck (_:ss) = fromList $ map read ss
 
-playRound :: (Queue Int, Queue Int) -> Maybe (Queue Int, Queue Int)
-playRound (p1, p2) = liftM2 playRound' (peek p1) (peek p2)
+playRound :: SeenDecks -> Decks -> Maybe Decks
+playRound seen (p1, p2)
+    | (p1, p2) `member` seen = Nothing
+    | otherwise              = liftM2 playRound' (peek p1) (peek p2)
   where
-    playRound' :: Int -> Int -> (Queue Int, Queue Int)
-    playRound' c1 c2
-      | c1 > c2   = (pushAll [c1, c2] (pop p1), pop p2)
-      | otherwise = (pop p1, pushAll [c2, c1] (pop p2))
+    playRound' :: Int -> Int -> Decks
+    playRound' c1 c2 = if p1Wins then (pushAll [c1, c2] nextP1, nextP2) else (nextP1, pushAll [c2, c1] nextP2)
+      where
+        nextP1 = pop p1
+        nextP2 = pop p2
+        p1Wins
+          | length nextP1 >= c1 && length nextP2 >= c2 = isP1Winner $ playGame (take c1 nextP1, take c2 nextP2)
+          | otherwise                                  = c1 > c2
 
-calculateScore :: (Queue Int, Queue Int) -> Int
+isP1Winner :: Decks -> Bool
+isP1Winner = not . null . fst
+
+calculateScore :: Decks -> Int
 calculateScore (p1, p2) = sum $ zipWith (*) [1..] $ reverse $ toList $ if null p1 then p2 else p1 
 
-playGame :: (Queue Int, Queue Int) -> (Queue Int, Queue Int)
-playGame decks = maybe decks playGame (playRound decks)
+playGame :: Decks -> Decks
+playGame decks = step Set.empty decks
+  where
+    step :: SeenDecks -> Decks -> Decks
+    step seen decks = maybe decks (step (insert decks seen)) (playRound seen decks)
 
 main :: IO ()
 main = do [p1, p2] <- readGroupsWith readDeck
--- 22/Queue.hs
+++ 22/Queue.hs
@@ -1,32 +1,49 @@
 module Queue where
 
+import Prelude hiding (drop, take)
+import qualified Prelude
 import Data.Foldable (Foldable, foldl')
+import Data.Maybe (catMaybes)
 
-data Queue a = Empty | Value [a] [a]
+
+data Queue a = Empty | Value Int [a] [a]
 
 instance Show a => Show (Queue a) where
-  show Empty = "=>[]=>"
-  show (Value ins outs) = "=>" ++ show (ins ++ reverse outs) ++ "=>"
+  show Empty = "<=[]<="
+  show q     = "<=" ++ show (toList q) ++ "<="
+
+instance Eq a => Eq (Queue a) where
+  q1 == q2 = toList q1 == toList q2
+
+instance Ord a => Ord (Queue a) where
+  compare q1 q2 = compare (toList q1) (toList q2)
 
 push :: a -> Queue a -> Queue a
 push x Empty            = singleton x
-push x (Value ins outs) = Value (x:ins) outs
+push x (Value len ins outs) = Value (len + 1) (x:ins) outs
 
 pushAll :: Foldable t => t a -> Queue a -> Queue a
 pushAll xs q = foldl' (flip push) q xs
 
 peek :: Queue a -> Maybe a
 peek Empty              = Nothing
-peek (Value _ (x:outs)) = Just x
+peek (Value _ _ (x:outs)) = Just x
 
 pop :: Queue a -> Queue a
-pop Empty                = Empty
-pop (Value [] [x])       = Empty
-pop (Value ins [x])      = Value [] (reverse ins)
-pop (Value ins (x:outs)) = Value ins outs
+pop Empty                    = Empty
+pop (Value _ [] [x])         = Empty
+pop (Value len ins [x])      = Value (len - 1) [] (reverse ins)
+pop (Value len ins (x:outs)) = Value (len - 1) ins outs
+
+take :: Int -> Queue a -> Queue a
+take n q = fromList $ Prelude.take n $ toList q
+
+drop :: Int -> Queue a -> Queue a
+drop 0 q = q
+drop n q = drop (n - 1) (pop q)
 
 singleton :: a -> Queue a
-singleton x = Value [] [x]
+singleton x = Value 1 [] [x]
 
 empty :: Queue a
 empty = Empty
@@ -37,7 +54,11 @@ null _     = False
 
 toList :: Queue a -> [a]
 toList Empty            = []
-toList (Value ins outs) = outs ++ reverse ins
+toList (Value _ ins outs) = outs ++ reverse ins
 
 fromList :: [a] -> Queue a
-fromList = Value []
+fromList list = Value (Prelude.length list) [] list
+
+length :: Queue a -> Int
+length Empty = 0
+length (Value len _ _) = len
